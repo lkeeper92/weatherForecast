06f1a2edd7c2737a8fb7675224dfaae25fd5887a
lkeeper92 <mini-syr@inbox.ru>
2025-01-31 13:24:10 +0300

    database MySQL added

--- main.go
+++ main.go
@@ -1,16 +1,49 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	_ "github.com/go-sql-driver/mysql"
 	"log"
 	"net/http"
 )
 
+var db *sql.DB
+
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+	rows, err := db.Query("SELECT name FROM users")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer rows.Close()
+
+	var names []string
+	for rows.Next() {
+		var name string
+		if err := rows.Scan(&name); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		names = append(names, name)
+	}
+
+	fmt.Fprintf(w, "Hi there, I love %s! Users: %v", r.URL.Path[1:], names)
 }
 
 func main() {
+	var err error
+	db, err = sql.Open("mysql", "root:password123@tcp(localhost:3306)/weather")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	http.HandleFunc("/", handler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
